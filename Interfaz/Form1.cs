319bc229c79337e25ca560021e812e7cb9aed81d
Ana Muñoz <munoz.am.118@gmail.com>
2020-04-03 19:50:44 -0500

    reversing from the good commit

--- Interfaz/Form1.cs
+++ Interfaz/Form1.cs
@@ -14,13 +14,12 @@ namespace Interfaz
         DataTable dt;
         private String url;
         private String addLink;
-        public static String FILTER1="?source=";
-        public static String FILTER2="?$select";
-        public static String FILTER3= "?$order";
-        public static String FILTER4= "?$offset";
+        public static String FILTER1 = "?source=";
+        public static String FILTER2 = "?$select";
+        public static String FILTER3 = "?$order";
+        public static String FILTER4 = "?$offset";
         public static String FILTER5 = "?$limit";
         public static String FILTER6 = "?$where";
-
         public Form1()
         {
             InitializeComponent();
@@ -42,8 +41,8 @@ namespace Interfaz
         {
             try
             {
-                List<String> p= Soda(urlText.Text, codeData.Text);
-                for(int i = 0; i < p.Count; i++)
+                List<String> p = Soda(urlText.Text, codeData.Text);
+                for (int i = 0; i < p.Count; i++)
                 {
                     comboBoxFilters.Items.Add(p[i]);
                 }
@@ -66,17 +65,36 @@ namespace Interfaz
                 Console.WriteLine("Something its wrong");
             }
 
-           
+
         }
-       
+
+
+
         private void searchData(object sender, EventArgs e)
-        {   
-           // dataGridView.DataSource = dt;
-
+        {
+            dt = new DataTable();
+            List<String> p = Soda(urlText.Text, codeData.Text);
+            for (int i = 0; i < p.Count; i++)
+            {
+                dt.Columns.Add(new DataColumn(p[i], typeof(String)));
+            }
             readInfo(urlText.Text);
-
-            dataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
             dataGridView.DataSource = dt;
+            dataGridView.Columns[1].Visible = true;
+            dataGridView.Columns[2].Visible = true;
+            dataGridView.Columns[3].Visible = true;
+            dataGridView.Columns[4].Visible = true;
+            dataGridView.Columns[5].Visible = true;
+            dataGridView.Columns[6].Visible = true;
+            dataGridView.Columns[7].Visible = true;
+            dataGridView.Columns[8].Visible = true;
+            dataGridView.Columns[0].Visible = true;
+            dataGridView.Columns[10].Visible = true;
+            dataGridView.Columns[11].Visible = true;
+            dataGridView.Columns[12].Visible = true;
+            dataGridView.Columns[13].Visible = true;
+            dataGridView.Columns[14].Visible = true;
+            dataGridView.Columns[15].Visible = true;
 
         }
         public List<String> Soda(String bdId, String code)
@@ -116,31 +134,20 @@ namespace Interfaz
             string result = "";
             try
             {
-                dt = new DataTable("Info");
-                List<String> p = Soda(urlText.Text, codeData.Text);
-                for (int i = 0; i < p.Count; i++)
-                {
-                    dt.Columns.Add(new DataColumn(p[i], typeof(String)));
-                    Console.WriteLine("Got it");
-                }
 
+                //var url = "https://www.datos.gov.co/resource/ysq6-ri4e.json?nombre_del_municipio=BARBOSA&variable=PM2.5&$limit=10&$offset=20";
                 var url = "https://www.datos.gov.co/resource/" + bdId + ".json?" + addLink + "&$limit=10&$offset=20";
                 var client = new WebClient();
                 using (var stream = client.OpenRead(url))
                 using (var reader = new StreamReader(stream))
                 {
-
-                  //  (line = reader.ReadLine()) != null
                     String line = reader.ReadLine();
                     int count = 0;
-                    while (count<p.Count)
+                    while ((line = reader.ReadLine()) != null)
                     {
                         Console.WriteLine("entro");
                         String[] args = line.Split(',');
-                        DataRow row = dt.NewRow();
-                        dt.Rows.Add(row[p[count]] = args[count]);
-                     
-                        //   dt.Rows.Add( "" + args[1], "" + args[2], "" + args[3], "" + args[4], "" + args[5], "" + args[6], "" + args[7], "" + args[8], "" + args[9], "" + args[10], "" + args[11], "" + args[12], "" + args[13], "" + args[14],"" +args[15],"" + args[16]);         
+                        dt.Rows.Add("" + args[1], "" + args[2], "" + args[3], "" + args[4], "" + args[5], "" + args[6], "" + args[7], "" + args[8], "" + args[9], "" + args[10], "" + args[11], "" + args[12], "" + args[13], "" + args[14], "" + args[15], "" + args[16]);
                         count++;
                     }
                     reader.Close();
@@ -172,8 +179,8 @@ namespace Interfaz
                 */
             }
             dataGridView1.DataSource = dt;
-            dt.Rows.Add("a","b");
-            
+            dt.Rows.Add("a", "b");
+
         }
 
         private void addButton_Click(object sender, EventArgs e)
@@ -182,7 +189,7 @@ namespace Interfaz
             String[] comboBoxSep = comboBoxMinus.Split(' ');
             comboBoxMinus = comboBoxSep.GetValue(0).ToString();
             String comboFixed;
-            for (int i=1; i<comboBoxSep.Length; i++)
+            for (int i = 1; i < comboBoxSep.Length; i++)
             {
                 comboBoxMinus = comboBoxMinus + "_" + comboBoxSep.GetValue(i);
             }
@@ -191,16 +198,16 @@ namespace Interfaz
             {
                 addLink = addLink + "&" + comboFixed + "=" + textBoxSearch.Text;
             }
-            else 
+            else
             {
                 addLink = comboFixed + "=" + textBoxSearch.Text;
             }
 
-            if (!filters.Text.Equals("Sin filtros")) 
+            if (!filters.Text.Equals("Sin filtros"))
             {
                 filters.Text = filters.Text + "\n" + comboBoxFilters.Text + ": " + textBoxSearch.Text;
             }
-            else 
+            else
             {
                 filters.Text = comboBoxFilters.Text + ": " + textBoxSearch.Text;
             }
@@ -208,9 +215,11 @@ namespace Interfaz
 
         private void button2_Click(object sender, EventArgs e)
         {
-            filters.Text = "Sin filtros";
-            dataGridView.Rows.Clear();
+              filters.Text = "Sin filtros";
+              dataGridView.Rows.Clear();
 
         }
     }
-}
+
+      
+    }
