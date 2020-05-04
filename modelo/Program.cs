5ddffebff30b28af43d6cc540e5f8d2cc9ec3be7
CristianLasso <crisca_25@hotmail.com>
2020-05-03 22:10:33 -0500

    Method removed

--- modelo/Program.cs
+++ modelo/Program.cs
@@ -78,46 +78,5 @@ namespace modelo
             }
 
         }
-
-        public static void LinearRegression(double[] xVals, double[] yVals,
-                                        out double rsquared, out double yintercept,
-                                        out double slope)
-        {
-            //Debug.Assert(xVals.Length == yVals.Length);
-            double sumOfX = 0;
-            double sumOfY = 0;
-            double sumOfXSq = 0;
-            double sumOfYSq = 0;
-            double ssX = 0;
-            double ssY = 0;
-            double sumCodeviates = 0;
-            double sCo = 0;
-            double count = 0;
-
-            for (int ctr = 0; ctr < xVals.Length; ctr++)
-            {
-                double x = xVals[ctr];
-                double y = yVals[ctr];
-                sumCodeviates += x * y;
-                sumOfX += x;
-                sumOfY += y;
-                sumOfXSq += x * x;
-                sumOfYSq += y * y;
-                count++;
-            }
-            ssX = sumOfXSq - ((sumOfX * sumOfX) / count);
-            ssY = sumOfYSq - ((sumOfY * sumOfY) / count);
-            double RNumerator = (count * sumCodeviates) - (sumOfX * sumOfY);
-            double RDenom = (count * sumOfXSq - (sumOfX * sumOfX))
-             * (count * sumOfYSq - (sumOfY * sumOfY));
-            sCo = sumCodeviates - ((sumOfX * sumOfY) / count);
-
-            double meanX = sumOfX / count;
-            double meanY = sumOfY / count;
-            double dblR = RNumerator / Math.Sqrt(RDenom);
-            rsquared = dblR * dblR;
-            yintercept = meanY - ((sCo / ssX) * meanX);
-            slope = sCo / ssX;
-        }
     }
 }
